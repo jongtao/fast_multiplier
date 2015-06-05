c3d07cd46d879b97f107de82a91c5cac3c90a8d9
J. Tao <jon.g.tao@gmail.com>
2015-06-05 00:11:56 -0700

    refactored CLA

--- cla.v
+++ cla.v
@@ -17,10 +17,10 @@ module CLA_64(
 	assign PP = P[0] & P[1] & P[2] & P[3];
 
 
-	CLA_16 add0(x[15:0], y[15:0], 1'b0, s[15:0], G[0], P[0]);
-	CLA_16 add1(x[31:16], y[31:16], c[0], s[31:16], G[1], P[1]);
-	CLA_16 add2(x[47:32], y[47:32], c[1], s[47:32], G[2], P[2]);
-	CLA_16 add3(x[63:48], y[63:48], c[2], s[63:48], G[3], P[3]);
+	CLA_16 add0(s[15:0], G[0], P[0], x[15:0], y[15:0], 1'b0);
+	CLA_16 add1(s[31:16], G[1], P[1], x[31:16], y[31:16], c[0]);
+	CLA_16 add2(s[47:32], G[2], P[2], x[47:32], y[47:32], c[1]);
+	CLA_16 add3(s[63:48], G[3], P[3], x[63:48], y[63:48], c[2]);
 endmodule // CLA_32
 
 
@@ -66,10 +66,10 @@ module CLA_16(
 		(G[0] & P[0] & P[1] & P[2] & P[3]);
 	assign PP = P[0] & P[1] & P[2] & P[3];
 
-	CLA_4 add0(x[3:0], y[3:0], 1'b0, s[3:0], G[0], P[0]);
-	CLA_4 add1(x[7:4], y[7:4], c[0], s[7:4], G[1], P[1]);
-	CLA_4 add2(x[11:8], y[11:8], c[1], s[11:8], G[2], P[2]);
-	CLA_4 add3(x[15:12], y[15:12], c[2], s[15:12], G[3], P[3]);
+	CLA_4 add0(s[3:0], G[0], P[0], x[3:0], y[3:0], 1'b0);
+	CLA_4 add1(s[7:4], G[1], P[1], x[7:4], y[7:4], c[0]);
+	CLA_4 add2(s[11:8], G[2], P[2], x[11:8], y[11:8], c[1]);
+	CLA_4 add3(s[15:12], G[3], P[3], x[15:12], y[15:12], c[2]);
 endmodule // CLA_16
 
 
@@ -85,17 +85,17 @@ module CLA_8(
 	//assign c = G[0] | (Cin & P[0]);
 	assign c = G[0]; // Cin is zero
 
-	CLA_4 add0(x[3:0], y[3:0], 1'b0, s[3:0], G[0], P[0]);
-	CLA_4 add1(x[7:4], y[7:4], c, s[7:4], G[1], P[1]);
+	CLA_4 add0(s[3:0], G[0], P[0], x[3:0], y[3:0], 1'b0);
+	CLA_4 add1(s[7:4], G[1], P[1], x[7:4], y[7:4], c);
 endmodule
 
 
 
 module CLA_4(
-	input [3:0] x, y,
-	input Cin,
 	output [3:0] s,
-	output GG,PP
+	output GG, PP,
+	input [3:0] x, y,
+	input Cin
 	);
 
 	wire [3:0] G,P;
@@ -112,15 +112,12 @@ module CLA_4(
 	assign P[3] = x[3] | y[3];
 
 	assign c[0] = G[0] | (Cin & P[0]);
-
 	assign c[1] = G[1] | (G[0] & P[1]) | (Cin & P[0] & P[1]); 
-
 	assign c[2] = G[2] | (G[1] & P[2]) | (G[0] & P[1] & P[2]) |
 		(Cin & P[0] & P[1] & P[2]);
 
 	assign GG = G[3] | (G[2] & P[3]) | (G[1] & P[2] & P[3]) |
 		(G[0] & P[1] & P[2] & P[3]);
-	
 	assign PP = P[0] & P[1] & P[2] & P[3];
 /*
 	assign c[3] = G[3] | (G[2] & P[3]) | (G[1] & P[2] & P[3]) |
